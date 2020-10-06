010d63bb044bce58e4f150a91497529fe6e36e1e
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-10-06 15:59:52 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,31 +11,13 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-  
   cart.map do |i|
     i.store(:count, 1)
   end
-  
   if cart[0].eql?(cart[1])
     cart.uniq
     cart[0][:count] += 1 
   end
-  
-  # cart.each do |i|
-  # if i.eql?(cart[1])
-  #   cart.uniq
-  # binding.pry
-  
-  # cart.each do |i|
-  #   if i[:item]
-  #     i[:count] += 1 
-  #   end
-  # end
-  cart
 end
 
 
